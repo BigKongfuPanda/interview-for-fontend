21815ff9f6771efd605ae19f1ce3637e7ce9226b
张海泉 <haiquan.zhang@tcredit.com>
2019-03-20 18:51:57 +0800

    update

--- 数据结构和算法/排序/1.快速排序.js
+++ 数据结构和算法/排序/1.快速排序.js
@@ -1,5 +1,6 @@
 /**
  * 快排：快排的原理是在数组中找一个基准值 base，创建两个数组 left 和 right，遍历整个数组，当当前元素小于等于基准值base的时候，放入左边数组left中，否则放入右边数组right中，继续按照上述方法分别递归数组left和数组right，直到left和right数组的元素小于等于1个的时候，返回合并的数组 [...qSort(left), ...[base], ...qSort(right)]
+ * 时间复杂度：快排的时间复杂度为O(nlogn)，最糟糕的时候是O(n^2)
  */
 
  function qSort(arr) {
