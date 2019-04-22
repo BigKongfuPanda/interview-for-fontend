2cc2096556c1badd462aad79019d8ce6e6981b24
zhanghaiquan <zhqustb@163.com>
2019-04-22 19:36:24 +0800

    update

--- 数据结构和算法/栈和队列/两个栈实现一个队列.js
+++ 数据结构和算法/栈和队列/两个栈实现一个队列.js
@@ -1,6 +1,6 @@
 /**
  * 思路：首先明确概念：栈是先进后出，有push和pop方法。队列是先进先出，队首进，队尾出
- * 定义两个栈 stack1 和 stack2。进队时，往stack1中push，出队时或者获取队列中第一个元素的时候，此时第一个元素在 stack1 的底部，暂时取不出来，所以先将 stack1 中的项目都push到 stack2 中，这样队列的第一个元素就在 stack2的顶部了
+ * 定义两个栈 stack1 和 stack2。进队时，往stack1中push，出队时或者获取队列中第一个元素的时候，此时第一个元素在 stack1 的底部，暂时取不出来，所以先将 stack1 中的元素都 push到 stack2 中，这样队列的第一个元素就在 stack2的顶部了
  */
 
 class Queue {
