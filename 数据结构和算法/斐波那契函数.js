3d8f4d637ca8396719dd6155b6121f24f84a98de
zhanghaiquan <zhqustb@163.com>
2021-05-04 22:45:41 +0800

    upd

--- 数据结构和算法/斐波那契函数.js
+++ 数据结构和算法/斐波那契函数.js
@@ -33,3 +33,14 @@ function fib(n) {
   }
   return list.pop();
 }
+
+// DP
+function fib(n) {
+  const dp = [];
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
